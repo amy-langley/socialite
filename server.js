87eed3c30711d777e782f8ccbe6c4d65f6001bb7
Amy Boyer <amy.r.boyer@gmail.com>
2016-03-04 14:20:12 -0600

    basic oauth is working

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 /* eslint no-console: 0 */
-import path from 'path';
-import express from 'express';
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from './webpack.config.js';
+import path from 'path'
+import express from 'express'
+import session from 'express-session'
+import webpack from 'webpack'
+import webpackMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+import config from './webpack.config.js'
+import Grant from 'grant-express'
 
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
-const app = express();
+const isDeveloping = process.env.NODE_ENV !== 'production'
+const port = isDeveloping ? 3000 : process.env.PORT
+const app = express()
+const grant = new Grant(require('./grant-config.json'))
+
+app.use(session({secret: 'shh dont tell', resave: false, saveUninitialized: false}))
+app.use(grant)
+
+app.get('/handle_tumblr_callback', function(req,res){
+  console.log(req.query);
+  res.end(JSON.stringify(req.query, null, 2))
+})
 
 if (isDeveloping) {
   const compiler = webpack(config);
@@ -23,24 +34,24 @@ if (isDeveloping) {
       chunkModules: false,
       modules: false
     }
-  });
+  })
 
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
+  app.use(middleware)
+  app.use(webpackHotMiddleware(compiler))
   app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-    res.end();
-  });
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')))
+    res.end()
+  })
 } else {
-  app.use(express.static(__dirname + '/dist'));
+  app.use(express.static(__dirname + '/dist'))
   app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+  })
 }
 
-app.listen(port, '0.0.0.0', function onStart(err) {
+app.listen(port, 'localhost', function onStart(err) {
   if (err) {
     console.log(err);
   }
-  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
-});
+  console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+})
