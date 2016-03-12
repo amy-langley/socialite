708fdccdc94faf6aa4f573f7f65b4ba1eb7cf882
Amy Boyer <amy.r.boyer@gmail.com>
2016-03-12 12:36:07 -0600

    clean up entry points and stuff

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'app/main.js')
+    path.join(__dirname, 'app/client/main.js')
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
