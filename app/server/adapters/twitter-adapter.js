b0408e8e4cac1113a43177b2b271e479ae0d5c4c
Hannah Hunter <felicette@icloud.com>
2016-03-20 13:42:24 -0500

    implements twitter npm plugin

--- app/server/adapters/twitter-adapter.js
+++ app/server/adapters/twitter-adapter.js
@@ -5,44 +5,49 @@ import GrantConfig from '../../config/grant-config.json';
 export default class TwitterAdapter {
 
   middleware(){
-    var self  = this
-    var router = express.Router()
+    var self  = this;
+    var router = express.Router();
 
-    router.get('/register', function(req,res){
-      req.session.credentials = req.session.credentials || {}
-      req.session.credentials.twitter = self.handleCredentials(req)
-      req.session.save( () => res.redirect('/home') )
-    })
+    router.get('/register', (req, res) => {
+      req.session.credentials = req.session.credentials || {};
+      req.session.credentials.twitter = self.handleCredentials(req);
+      req.session.save( () => res.redirect('/home') );
+    });
 
-    router.get('/posts/:p', self.fetchPosts)
+    router.get('/posts/:p', self.fetchPosts);
 
-    return router
+    return router;
   }
 
-  handleCredentials(req){
+  handleCredentials(req) {
     return {
-      consumer_key: grantconfig.twitter.key,
-      consumer_secret: grantconfig.twitter.secret,
-      token: req.query.access_token,
-      token_secret: req.query.access_secret
-    }
+      consumer_key: GrantConfig.twitter.key
+			, consumer_secret.twitter.secret
+			, token: req.query.access_token
+			, token_secret: req.query.access_secret
+    };
   }
 
-  fetchPosts(req,res){
-    var blog = req.params.p
-    var credentials = req.session.credentials ?
-      req.session.credentials['twitter'] :
-      null
+  fetchPosts(req, res) {
+    var credentials = req.session.credentials;
 
-    if(!credentials){
-      res.status(500).send('Not logged in')
-      return
+    if(!credentials) {
+      res.status(500).send('Not logged in');
+      return;
     }
+    
+		var twitter = new Twitter(credentials);
 
-    var twitter = twitter.createClient(credentials)
-    twitter.posts(blog, (err, resp) => {
-      if(err) res.status(500).send(JSON.stringify(err))
-      else res.send(JSON.stringify(resp.posts))
-    })
-  }
-}
+    // var blog = req.params.p;
+    // var credentials = req.session.credentials ?
+    //   req.session.credentials['twitter'] :
+    //   null;
+
+
+    // var twitter = twitter.createClient(credentials);
+    // twitter.posts(blog, (err, resp) => {
+    //   if(err) res.status(500).send(JSON.stringify(err))
+    //   else res.send(JSON.stringify(resp.posts))
+    });
+  };
+};
