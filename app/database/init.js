cd9db8231fdf094b9b135a4b72a887e17980edd2
Amy Boyer <amy.r.boyer@gmail.com>
2016-03-21 23:08:09 -0500

    slightly less trivial data

--- app/database/init.js
+++ app/database/init.js
@@ -52,18 +52,32 @@ LinkedAccount.belongsTo(User);
 
 User.sync({force: true}).then(function () {
   // Table created
-  return User.create({
+  User.create({
     username: 'admin',
     password: sha1(interleave('password',salt)),
     firstName: 'Admin',
     lastName: 'User'
   });
+  User.create({
+    username: 'nobody',
+    password: ''
+  })
 }).then(function(){
   LinkedAccount.sync({force: true}).then(function(){
-    return LinkedAccount.create({
+    LinkedAccount.create({
       userId: 1,
       service: 'tumblr',
       username: 'umsweatie'
+    })
+    LinkedAccount.create({
+      userId: 1,
+      service: 'tumblr',
+      username: 'just-discourse-things'
+    })
+    LinkedAccount.create({
+      userId: 2,
+      service: 'twitter',
+      username: 'twitter'
     })
   }).then(function(){
     return User.findAll({where: {id: 1}, include: [{model: LinkedAccount}]})
