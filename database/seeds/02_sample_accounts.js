193b3d2ded44bbe91d67c66fa1e64957e621ea88
Felicity Lovelace <felicette@icloud.com>
2016-05-12 19:55:19 -0500

    Adds facebook sample linked account to account seed file

--- database/seeds/02_sample_accounts.js
+++ database/seeds/02_sample_accounts.js
@@ -1,4 +1,3 @@
-
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
@@ -8,6 +7,9 @@ exports.seed = function(knex, Promise) {
     knex('linked_accounts').insert({user_id: 1, service: 'tumblr', username: 'umsweatie', token: '', secret: ''}),
     knex('linked_accounts').insert({user_id: 1, service: 'tumblr', username: 'just-discourse-things', token: '', secret: ''}),
     knex('linked_accounts').insert({user_id: 1, service: 'twitter', username: 'umsweatie', token: '', secret: ''}),
-    knex('linked_accounts').insert({user_id: 2, service: 'twitter', username: 'slimegirlfriend', token: '', secret: ''})
+    knex('linked_accounts').insert({user_id: 2, service: 'twitter', username: 'slimegirlfriend', token: '', secret: ''}),
+    knex('linked_accounts').insert({ user_id: 1
+                                   , service: "facebook"
+                                   , username: 'kitty.a.lovelace' })
   );
 };
